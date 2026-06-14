8bb07e28f87ceb8d64a04fd154bb6bd34c7a0b90
Rishita Chakravorty <chakravorty.r20@outlook.com>
2026-06-14 09:31:51 +0530

    server start line changed

--- EmotionDetector/main.py
+++ EmotionDetector/main.py
@@ -80,6 +80,7 @@ def stop():
         return {
             "error": str(e)
         }
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    port = int(os.environ.get("PORT", 8080)) 
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
